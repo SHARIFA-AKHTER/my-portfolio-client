080139289b329129de7f61fe230a491ec554ce85
unknown <sr0589071@gmail.com>
2026-01-14 23:33:51 +0600

    updated an middlewere ts file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
- 
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
 
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-//   callbacks: {
-//        async signIn({account }) {
-//       if (account?.provider === "google") {
-//         return true; 
-//       }
-//       return true;
-//     },
-//     async jwt({ token, account }) {
-//       if (account?.id_token) {
-//         token.idToken = account.id_token;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }: { session: any; token: any }) {
-//       session.idToken = token.idToken;
-//       return session;
-//     },
-//   },
-//   secret: process.env.AUTH_SECRET,
-//   debug: true, 
-// });
-
-// export { handler as GET, handler as POST };
 
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,34 @@
+// import { NextResponse, NextRequest } from "next/server";
+
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get("token")?.value;
+//   const url = request.nextUrl.clone();
+
+//   if (!token) {
+//     url.pathname = "/login";
+//     return NextResponse.redirect(url);
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/dashboard/:path*"],
+// };
 import { NextResponse, NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
+ 
+  if (request.nextUrl.pathname.startsWith('/api/auth')) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get("next-auth.session-token")?.value || request.cookies.get("__Secure-next-auth.session-token")?.value;
   const url = request.nextUrl.clone();
 
-  if (!token) {
+
+  if (request.nextUrl.pathname.startsWith('/dashboard') && !token) {
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
@@ -14,5 +37,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+ 
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
