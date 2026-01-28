fabf28080e67cdc34cf18797bd115826003d8c0e
unknown <sr0589071@gmail.com>
2026-01-29 01:55:18 +0600

    create an project view page

--- services/BlogServices/index.ts
+++ services/BlogServices/index.ts
@@ -3,19 +3,49 @@ export const getBlogById = async (idOrSlug: string) => {
   return await res.json();
 };
 
+// export const getProjectById = async (id: string | number) => {
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/projects/${id}`, {
+//       cache: "no-store",
+//     });
+
+//     if (!res.ok) return null;
+
+//     const result = await res.json();
+    
+//     return result
+//   } catch (err) {
+//     console.error("Fetch error:", err);
+//     return null;
+//   }
+// };
+
+// export const getProjectById = async (id: string | number) => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/projects/${id}`);
+//   if (!res.ok) return null;
+//   return await res.json();
+// };
+
 export const getProjectById = async (id: string | number) => {
   try {
+  
+    if (!id) return null;
+
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/projects/${id}`, {
       cache: "no-store",
     });
 
-    if (!res.ok) return null;
+    if (!res.ok) {
+      console.error(`API response error: ${res.status}`);
+      return null;
+    }
 
     const result = await res.json();
     
-    return result
+    return result.data || result; 
+
   } catch (err) {
-    console.error("Fetch error:", err);
+    console.error("Fetch error details:", err);
     return null;
   }
 };
