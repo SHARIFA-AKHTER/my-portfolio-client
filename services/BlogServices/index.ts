b7d3851fb893291d87add523f5c74b1ba1bd5378
unknown <sr0589071@gmail.com>
2026-01-30 23:30:17 +0600

    folder sracture update

--- services/BlogServices/index.ts
+++ services/BlogServices/index.ts
@@ -5,20 +5,44 @@ export const getBlogById = async (idOrSlug: string) => {
   return await res.json();
 };
 
-export const getProjectById = async (id: string | number) => {
+// export const getProjectById = async (id: string | number) => {
  
+//   try {
+//     const res = await fetch(
+//       `${process.env.NEXT_PUBLIC_BASE_API}/projects/${id}`,
+//       {
+//         cache: "no-store",
+//       },
+//     );
+//     const result = await res.json();
+//     return result.success ? result.data : result;
+//   } catch (err) {
+//     console.error("API Fetch Error:", err);
+//     return null;
+
+//   }
+// };
+
+export const getProjectById = async (id: string | number) => {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_API}/projects/${id}`,
-      {
-        cache: "no-store",
-      },
+      { cache: "no-store" }
     );
+    if (!res.ok) return null;
     const result = await res.json();
-    return result.success ? result.data : result;
+    
+    // পোস্টম্যান অনুযায়ী যদি success true হয়, তবে সরাসরি data অবজেক্টটি রিটার্ন করবে
+    return result.success ? result.data : result; 
   } catch (err) {
     console.error("API Fetch Error:", err);
     return null;
-
   }
 };
+// export const getProjectById = async (id: string) => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/projects/${id}`, {
+//     cache: "no-store",
+//   });
+//   const result = await res.json();
+//   return result.data; 
+// };
